02a73a51e005f3c17d490dad7b7b94836de0e5f0
Yehezkiel David Setiawan <yehezkieldavid2006@gmail.com>
2023-07-26 09:29:19 +0700

    Autentikasi Login

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -1,12 +1,21 @@
 package com.example.controller;
-
+import com.example.demo.HelloApplication;
+import com.example.entity.User;
+import com.example.auth.LoginService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 
 public class LoginController {
-
     @FXML
     private TextField usernameField;
 
@@ -14,13 +23,51 @@ public class LoginController {
     private PasswordField passwordField;
 
     @FXML
-    private void handleLogin(ActionEvent event) {
+    private Button loginButton;
+
+    private Stage stage;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    @FXML
+    private void handleLoginButtonAction() {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
-        // Here you can implement the logic to validate the username and password
-        // For simplicity, we'll just print them for demonstration purposes.
-        System.out.println("Username: " + username);
-        System.out.println("Password: " + password);
+        LoginService loginService = new LoginService();
+        boolean isAuthenticated = loginService.authenticate(username, password);
+
+        if (isAuthenticated) {
+
+            System.out.println("Login berhasil!");
+
+            User user = loginService.getUserByUsername(username);
+            System.out.println("Selamat datang, " + user.getNama() + "!");
+
+                try {
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/demo/pesantiket.fxml"));
+                    Parent dashboardRoot = fxmlLoader.load();
+                    PesanTiketController dashboardController = fxmlLoader.getController();
+
+                    Stage dashboardStage = new Stage();
+                    dashboardStage.setScene(new Scene(dashboardRoot));
+
+                    dashboardStage.show();
+                    if (loginButton.getScene() != null) {
+                        loginButton.getScene().getWindow().hide();
+                    }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Login gagal! Periksa kembali username dan password Anda.");
+        }
     }
+
+
+
+
 }
